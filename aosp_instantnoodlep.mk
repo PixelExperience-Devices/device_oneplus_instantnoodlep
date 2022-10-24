833dab18f5234930a5ebcba998e5ea70360ff496
Shantanu Sarkar <shantanuplussarkar@gmail.com>
2022-10-25 00:18:35 +0530

    instantnoodlep: Ship along with OnePlus firmware
    Change-Id: I0cf47621f39133ce5e41622b27fde72cef7414e0

--- aosp_instantnoodlep.mk
+++ aosp_instantnoodlep.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from instantnoodlep device
 $(call inherit-product, device/oneplus/instantnoodlep/device.mk)
 
+# Inherit OnePlus firmware
+$(call inherit-product, vendor/oneplus/instantnoodlep/Android.mk)
+
 # Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
